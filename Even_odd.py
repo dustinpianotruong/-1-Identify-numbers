e46a34013dba5939179b430464b60a260abf8f98
dustinpianotruong <dustinpianotruong@gmail.com>
2026-05-30 17:50:36 -0700

    Update Even_odd.py

--- Even_odd.py
+++ Even_odd.py
@@ -1,10 +1,12 @@
 #Instruction: Write a simple program asking for a number and identify whether the number is even or odd. Keep the record of those numbers.
+#Store the number
+Store = []
 print("Type a number")
 #Ask user to type in number
+while(num != "Stop"):
 num = int(input())
 #Checking whether even or odd
 if num % 2 == 0:
   print("Even")
 else:
   print("Odd")
-#Store the number
